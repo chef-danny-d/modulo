049c7aced932c8e47dad1fffc82ce531171e631c
Dániel B. Papp <dpapp@odu.edu>
2023-04-18 09:53:15 -0400

    fix(query): Addressed type safety errors

--- src/courses/hooks/useCourse.ts
+++ src/courses/hooks/useCourse.ts
@@ -1,11 +1,19 @@
 import { useQuery } from '@blitzjs/rpc'
 import { Prisma } from 'db'
 import getCourseBySlug from '../queries/getCourseBySlug'
+import { Course, User, Chapter, Lesson } from '@prisma/client'
 
-export const useCourse = (slug: string, args?: Prisma.CourseFindManyArgs) => {
+export const useCourse = (slug: string, args: Prisma.CourseFindManyArgs = {}) => {
 	const [course] = useQuery(getCourseBySlug, {
 		slug,
 		...args,
 	})
-	return course
+	return course as Course & {
+		author: User
+		chapters: Array<
+			Chapter & {
+				lessons: Array<Lesson>
+			}
+		>
+	}
 }
--- src/courses/queries/getCourseBySlug.ts
+++ src/courses/queries/getCourseBySlug.ts
@@ -6,32 +6,13 @@ import { z } from 'zod'
 const GetCourse = z.object({
 	// This accepts type of undefined, but is required at runtime
 	slug: z.string().optional().refine(Boolean, 'Required'),
-	include: z.object({
-		orderBy: z.array(
-			z.object({
-				position: z.enum(['asc', 'desc']),
-			})
-		),
-		chapters: z.object({
-			include: z.object({
-				lessons: z.object({
-					orderBy: z.array(
-						z.object({
-							position: z.enum(['asc', 'desc']),
-						})
-					),
-				}),
-			}),
-		}),
-		author: z.boolean(),
-	}),
+	include: z.nullable(z.object({}).optional()),
 })
 
 export default resolver.pipe(
 	resolver.zod(GetCourse),
 	resolver.authorize(),
 	async ({ slug, ...rest }) => {
-		console.log(rest)
 		// TODO: in multi-tenant app, you must add validation to ensure correct tenant
 		const course = await db.course.findFirst({
 			where: { slug },
